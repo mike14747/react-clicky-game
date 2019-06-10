faac6be8c18008d5ab48dc15de30ad0fe2c85df3
mike4747 <mike4747@oh.rr.com>
2019-06-09 23:21:45 -0400

    updated padding and font size in header and instructions components

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,11 +4,11 @@ import './headerStyles.css';
 
 export default function Header(props) {
     return (
-        <nav className="sticky-top navStyles">
+        <nav className="sticky-top navStyles py-2">
             <ul className="d-block m-0 p-0">
-                <li className="h4 brandAlign liStyles">19th Century Presidents</li>
-                <li className="h5 text-center liStyles">{props.message}</li>
-                <li className="h4 counterAlign liStyles"><Counter score={props.score} topScore={props.topScore} /></li>
+                <li className="h4 brandAlign liStyles py-2">19th Century Presidents</li>
+                <li className="h5 text-center liStyles py-2">{props.message}</li>
+                <li className="h4 counterAlign liStyles py-2"><Counter score={props.score} topScore={props.topScore} /></li>
             </ul>
         </nav>
     );
--- src/components/Instructions/instructions.js
+++ src/components/Instructions/instructions.js
@@ -4,7 +4,7 @@ import './instructionsStyles.css';
 export default function Instructions() {
 
     return (
-        <div className="instFont instructionsDiv p-4 mb-4">
+        <div className="instFont instructionsDiv p-3 mb-4">
             Click on each 19th century president sequentially, without clicking on any of them twice, to win the game!
         </div>
     );

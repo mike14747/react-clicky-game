af6a814ebb2e867ef9fc52585178437b782b4400
mike4747 <mike4747@oh.rr.com>
2019-06-09 22:21:40 -0400

    Updates

---
+++ precache-manifest.2526884a0bff44581a23e6dedefc50c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b15c731fd7fcabe065c2e918e2f9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d29dd16ce39b657e6a9",
+    "url": "/static/css/main.d210a4ba.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "1d29dd16ce39b657e6a9",
+    "url": "/static/js/main.1fa29b92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3389913653e4a2cc0a055a0997bf45d",
+    "url": "/static/media/parchment.f3389913.jpg"
+  }
+]);
